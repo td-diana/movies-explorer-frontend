98c00d9057b7b8486b9e4ac6647e340c1f00486a
td-diana <99271477+td-diana@users.noreply.github.com>
2022-10-22 15:20:54 +0300

    компонент header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,33 @@
-import './Header.css';
+import { Link } from "react-router-dom";
+import "./Header.css";
+import logo from "../../images/logo-movies-explorer-blue.svg";
 
 function Header() {
   return (
-    <div className="header">
-      
-    </div>
+    <header className="header">
+      <div className="header__top-bar">
+        <Link to="/">
+          <img className="header__logo" src={logo} alt="логотип" />
+        </Link>
+        <nav className="header__navbar">
+          <ul className="header__navbar-list">
+            <li>
+              <Link to="/signup" className="header__navbar-link">
+                Регистрация
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/signin"
+                className="header__navbar-link header__navbar-link_signin"
+              >
+                Войти
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 }
 
---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import "./Main.css";
+import logo from "../../images/promo-logo.svg";
+
+function Main() {
+  return (
+    <main className="main">
+      <img src={logo} className="promo__logo" alt="" />
+    </main>
+  );
+}
+
+export default Main;
